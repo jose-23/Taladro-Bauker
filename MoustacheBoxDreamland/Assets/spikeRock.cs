b6111d11a09135ead301e0cad2f0368ba11574a3
jose-23 <jose.flores02@alumnos.ucn.cl>
2020-08-27 19:15:49 -0400

    SpikeRock

---
+++ MoustacheBoxDreamland/Assets/spikeRock.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Diagnostics;
+using UnityEngine;
+
+public class spikeRock : MonoBehaviour
+{
+    //parte spike
+   /* private void OnCollisionEnter2D(Collision2D collision) {
+        if (collision.transform.CompareTag("Player")) {
+            Debug.Log("Player Damaged");
+            Destroy(collision.GameObject);
+        }
+    }*/
+}
