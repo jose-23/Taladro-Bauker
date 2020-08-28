6f4cbe4618c69c7c8427ad955867644d5a0980e1
jose-23 <jose.flores02@alumnos.ucn.cl>
2020-08-28 11:51:13 -0400

    bananas prueba mapa 1

---
+++ MoustacheBoxDreamland/Assets/fruitCollected.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class fruitCollected : MonoBehaviour
+{
+    //public GameObject obj;
+    private void OnTriggerEnter2D(Collider2D col) {
+        if (col.gameObject.tag == "Player") {
+            //obj.gameObject.SetActive(true);
+            //Destroy(gameObject);
+            GetComponent<SpriteRenderer>().enabled = false;
+        }
+    }
+}
