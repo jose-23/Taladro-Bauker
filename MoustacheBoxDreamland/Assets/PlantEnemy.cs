1673fd5ad6287691a50f46c694a46f0419b22c74
jose-23 <jose.flores02@alumnos.ucn.cl>
2020-08-29 22:10:13 -0400

    Plant lista

--- MoustacheBoxDreamland/Assets/PlantEnemy.cs
+++ MoustacheBoxDreamland/Assets/PlantEnemy.cs
@@ -8,6 +8,26 @@ public class PlantEnemy : MonoBehaviour
     public float waitTimeToAttack = 3; //tiempo de cada bala
     public Animator animator;
     public GameObject bulletPrefab;
-    public Transform lauchSpawnPoint; //posicion de lanzamiento
+    public Transform launchSpawnPoint; //posicion de lanzamiento
 
+    private void Start() {
+        waitedTime = waitTimeToAttack;
+    }
+
+    private void Update() {
+        if (waitedTime <= 0)
+        {
+            waitedTime = waitTimeToAttack;
+            animator.Play("Attack");
+            Invoke("LaunchBullet",0.5f);
+        }
+        else {
+            waitedTime -= Time.deltaTime;
+        }
+    }
+
+    public void LaunchBullet() {
+        GameObject newBullet;
+        newBullet = Instantiate(bulletPrefab,launchSpawnPoint.position,launchSpawnPoint.rotation);
+    }
 }
