354d6318578c6d6fa7f0c73c422c23f921ea982a
marcelo-23 <marcelo.manrique@alumnos.ucn.cl>
2020-08-29 21:54:30 -0400

    Avance Menu

---
+++ MoustacheBoxDreamland/Assets/Menu/Script/IntroScreenBehaviour.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class IntroScreenBehaviour : MonoBehaviour
+{
+    public GameObject optionsMenu;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.anyKey == true)
+        {
+            //pasar a la pantalla de opciones
+            optionsMenu.SetActive(true);
+            gameObject.SetActive(false);
+        }
+        
+    }
+}
