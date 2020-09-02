1325f6ce6a224dee55848af68e57f7d8b3118318
marcelo-23 <marcelo.manrique@alumnos.ucn.cl>
2020-09-01 22:54:57 -0400

    Boton Pausa
    tiene un error por si aca
    https://www.youtube.com/watch?v=7EYi-qpYkOU

---
+++ MoustacheBoxDreamland/Assets/Menu/Script/UiManager.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class UiManager : MonoBehaviour
+{
+    public GameObject optionsPanel;
+
+    public void OptionsPanel()
+    {
+        Time.timeScale = 0;
+        optionsPanel.SetActive(true);
+        print("hola");
+
+    }
+
+    public void Return()
+    {
+        Time.timeScale = 1;
+        optionsPanel.SetActive(false);
+
+
+    }
+
+    public void AnotherOptions()
+    {
+        //sound
+        //graphics
+    }
+    public void GoMainMenu()
+    {
+        SceneManager.LoadScene("Menu");
+    }
+    public void QuitGame()
+    {
+        Application.Quit();
+
+    }
+}
